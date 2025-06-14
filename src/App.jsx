e28bdf6951f5d51cb1b55f9c9bdc4b9907c2bc64
Lasara2001 <sandunilasara2001@gmail.com>
2025-06-14 14:03:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import SkillsSection from './components/SkillsSection'; // Fixed import name
+import Skillssection from './components/Skillssection'; // Fixed import name
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<SkillsSection />} /> {/* Fixed component name */}
+          <Route path="/skills" element={<Skillssection />} /> {/* Fixed component name */}
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
